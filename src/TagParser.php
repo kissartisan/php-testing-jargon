70fd15aad064b92551e632bd0b1031c23d842d4f
kissartisan <kissartisan@pm.me>
2023-06-16 13:36:17 +0800

    Ep. 4 > When to Reach for Data Providers

--- src/TagParser.php
+++ src/TagParser.php
@@ -6,7 +6,7 @@ class TagParser
 {
     public function parse(string $tags): array
     {
-        return preg_split('/ ?[,|] ?/', $tags);
+        return preg_split('/ ?[,|!] ?/', $tags);
         // return array_map(fn($tag) => trim($tag), $tags);
     }
 }
--- tests/TagParserTest.php
+++ tests/TagParserTest.php
@@ -6,53 +6,27 @@ use PHPUnit\Framework\TestCase;
 
 class TagParserTest extends TestCase
 {
-    protected TagParser $parser;
-
-    protected function setUp(): void
+    /**
+     * @dataProvider tagsProvider
+     */
+    public function test_it_parses_tags($input, $expected)
     {
-        // Given / Arrange
-        $this->parser = new TagParser();
-    }
+        $parser = new TagParser();
 
-    // personal, money, family
-    public function test_it_parses_a_single_tag()
-    {
-        // When / Act
-        $result = $this->parser->parse('personal');
-        $expected = ['personal'];
-
-        // Then / Assert
-        $this->assertSame($expected, $result);
-    }
-
-    public function test_it_parses_a_comma_separated_list_of_tag()
-    {
-
-        $result = $this->parser->parse('personal, money, family');
-        $expected = ['personal', 'money', 'family'];
+        $result = $parser->parse($input);
 
         $this->assertSame($expected, $result);
     }
 
-    public function test_it_parses_a_pipe_separated_list_of_tags()
+    public static function tagsProvider()
     {
-
-        $result = $this->parser->parse('personal | money | family');
-        $expected = ['personal', 'money', 'family'];
-
-        $this->assertSame($expected, $result);
+        return [
+            ['personal', ['personal']],
+            ['personal, money, family', ['personal', 'money', 'family']],
+            ['personal,money,family', ['personal', 'money', 'family']],
+            ['personal | money | family', ['personal', 'money', 'family']],
+            ['personal|money|family', ['personal', 'money', 'family']],
+            ['personal!money!family', ['personal', 'money', 'family']],
+        ];
     }
-
-    public function test_spaces_are_optional()
-    {
-
-        $result = $this->parser->parse('personal,money,family');
-        $expected = ['personal', 'money', 'family'];
-        $this->assertSame($expected, $result);
-
-        $result = $this->parser->parse('personal|money|family');
-        $expected = ['personal', 'money', 'family'];
-        $this->assertSame($expected, $result);
-    }
-
 }
